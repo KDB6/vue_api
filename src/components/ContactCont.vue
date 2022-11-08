c49c611d6a1b26c7fbc72cc8a2b22404800a0746
KDB6 <ipkg7210@gmail.com>
2022-11-08 13:01:44 +0900

    11 08
    11 08

---
+++ src/components/ContactCont.vue
@@ -0,0 +1,7 @@
+<template>
+  <section className="cont__contact">
+    <p>Movie</p>
+    <h3>Movie<em>Api</em></h3>
+    <a href="mailto:ipkg7210@gmail.com">ipkg7210@gmail.com</a>
+  </section>
+</template>
